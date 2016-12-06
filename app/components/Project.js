94c745521d06fb4a2c2af9feca1fc5aebe372202
sakomoko <sakomoko@gmail.com>
2016-12-06 16:21:11 +0900

    ProjectとFileにコンポーネントを分割した。

--- app/components/Project.js
+++ app/components/Project.js
@@ -1,27 +1,13 @@
 // @flow
 import React, { Component, PropTypes } from 'react';
-import Avatar from 'material-ui/Avatar';
-import { Card, CardHeader } from 'material-ui/Card';
-import Description from 'material-ui/svg-icons/action/description';
 // import { Link } from 'react-router';
+import File from './File';
 import ProjectRecord from '../models/Project';
-import TextFileRecord from '../models/TextFile';
 import styles from './Project.css';
-
-import {
-  blue300, cyanA700
-} from 'material-ui/styles/colors';
 
 export default class Project extends Component {
   static propTypes = {
     project: PropTypes.instanceOf(ProjectRecord).isRequired
-  }
-
-  getChangedClass(item: TextFileRecord): string {
-    if (item.get('isChange')) {
-      return 'animated pulse';
-    }
-    return '';
   }
 
   render() {
@@ -32,16 +18,7 @@ export default class Project extends Component {
           <h3>Project: {project.get('directoryPath')}</h3>
           <ul>
             {project.get('files').map(item =>
-              <li className={styles.list} key={item.get('fileName')}>
-                <Card className={this.getChangedClass(item)}>
-                  <CardHeader
-                    avatar={<Avatar icon={<Description />} backgroundColor={blue300} />}
-                    title={item.get('fileName')}
-                    subtitle={<span><span style={{color: cyanA700}}>{item.get('page')}</span><span>{item.getDifferencePageString()}</span>{` pages, ${item.get('overLine')} line; Total ${item.get('length').toLocaleString()}${item.getDifferenceLengthString()} char.`}</span>}
-                    titleStyle={{fontSize: '18px', marginBottom: '8px'}}
-                   />
-                </Card>
-              </li>
+              <File item={item} />
             )}
           </ul>
         </div>
