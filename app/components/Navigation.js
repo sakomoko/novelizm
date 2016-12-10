ed3a691b307c8c78dc59a40d4df0b46f0a4a44a2
AIZAWA Sako <sakomoko@gmail.com>
2016-12-10 13:39:30 +0900

    設定アイコンを追加

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -4,6 +4,7 @@ import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import ProjectRecord from '../models/Project';
 import FolderOpen from 'material-ui/svg-icons/file/folder-open';
+import SettingIcon from 'material-ui/svg-icons/action/settings';
 
 export default class Navigation extends Component {
   static propTypes = {
@@ -18,6 +19,7 @@ export default class Navigation extends Component {
         title={project.getDirectoryName() || 'novelizm'}
         titleStyle={{fontSize: '24px'}}
         iconElementLeft={<IconButton onClick={openDirectory}><FolderOpen /></IconButton>}
+        iconElementRight={<IconButton><SettingIcon /></IconButton>}
        />
     );
   }
